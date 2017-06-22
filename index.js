862cb2cc437f9823dbe8866b436c0245959435aa
kmfern808 <kmfern808@gmail.com>
2017-06-21 19:38:12 -0700

    added content and css

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path');
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/styles.css'));
 });
 
 app.listen(8080);
